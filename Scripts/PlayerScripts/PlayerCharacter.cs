dfc2d2ba9429c5e53523c71b5c5cf393cc8c28f7
clearskky <35397726+clearskky@users.noreply.github.com>
2020-08-09 10:59:56 +0300

    First Commit

---
+++ Scripts/PlayerScripts/PlayerCharacter.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+    public float JumpForce;
+    public float MaxHorizontalVelocity;
+    public float MaxVerticalVelocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimatePlayer();
+    }
+
+    private void AnimatePlayer()
+    {
+        //Vector2 characterScale = transform.localScale;
+        //if (rb.velocity.x < 0)
+        //{
+        //    characterScale.x = -1;
+        //}
+        //if (rb.velocity.x > 0)
+        //{
+        //    characterScale.x = 1;
+        //}
+        //transform.localScale = characterScale;
+
+        animator.SetFloat("verticalVelocity", rb.velocity.y);
+        animator.SetFloat("horizontalVelocity", rb.velocity.x);
+    }
+
+    public void Jump(Vector2 direction)
+    {
+        rb.AddForce(direction * JumpForce, ForceMode2D.Impulse);
+    }
+
+    private Vector2 NormalizeJumpVelocity(Vector2 velocity)
+    {
+        if (velocity.x > MaxHorizontalVelocity)
+        {
+            velocity.x = MaxHorizontalVelocity;
+        }
+        else if (velocity.x < (MaxHorizontalVelocity * (-1)))
+        {
+            velocity.x = (MaxHorizontalVelocity * (-1));
+        }
+
+        if (velocity.y > MaxVerticalVelocity)
+        {
+            velocity.y = MaxVerticalVelocity;
+        }
+        else if (velocity.y < (MaxVerticalVelocity * (-1)))
+        {
+            velocity.y = (MaxVerticalVelocity * (-1));
+        }
+        Debug.Log(velocity);
+        return velocity;
+    }
+}
---
+++ Scripts/UtilityScripts/TouchManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchManager : MonoBehaviour
+{
+    public Text phaseDisplayText;
+    public Text velocityDisplayText;
+    private Touch theTouch;
+    public Vector2 touchStartPosition, touchEndPosition, direction;
+    public PlayerCharacter playerCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            theTouch = Input.GetTouch(0);
+            if (theTouch.phase == TouchPhase.Began)
+            {
+                touchStartPosition = theTouch.position;
+            }
+            else if (theTouch.phase == TouchPhase.Ended)
+            {
+                touchEndPosition = theTouch.position;
+
+                float x = touchStartPosition.x - touchEndPosition.x;
+                float y = touchStartPosition.y - touchEndPosition.y;
+                direction = new Vector2(x, y);
+                playerCharacter.Jump(direction);
+            }
+            
+            phaseDisplayText.text = theTouch.phase.ToString();
+
+            
+        }
+
+    }
+}
