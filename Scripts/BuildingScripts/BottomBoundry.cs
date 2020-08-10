5025640299b6202dfa4fd30dfa72be39e2977d2a
clearskky <35397726+clearskky@users.noreply.github.com>
2020-08-10 17:25:15 +0300

    Added missing files

---
+++ Scripts/BuildingScripts/BottomBoundry.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomBoundry : MonoBehaviour, IBoundry
+{
+    public Rigidbody2D playerRB;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerRB.velocity = Vector2.zero;
+    }
+}
---
+++ Scripts/BuildingScripts/SideBoundry.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class SideBoundry : MonoBehaviour, IBoundry
+{
+    public PlayerCharacter Player;
+    public float PushForce;
+    private Vector2 PushDirection;
+
+    
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        PushDirection = new Vector2(collision.contacts[0].point.x - Player.transform.position.x, 0);
+        PushDirection = (-1) * PushDirection.normalized * PushForce;
+
+        Rigidbody2D playerRB = Player.GetComponent<Rigidbody2D>();
+        playerRB.AddForce(PushDirection * PushForce, ForceMode2D.Impulse);
+        Debug.Log("Back off");
+    }
+}
